6185092c213577989542339842a571d1083b1c0d
Neil C Smith <neil@codelerity.com>
2020-10-23 15:10:46 +0100

    Assert Caps size.

--- test/org/freedesktop/gstreamer/SampleTest.java
+++ test/org/freedesktop/gstreamer/SampleTest.java
@@ -46,10 +46,12 @@ public class SampleTest {
     public void testGetCaps() {
     	SampleTester.test((Sample sample) -> {
     		Caps caps = sample.getCaps();
+                assertEquals(1, caps.size());
     		Structure struct = caps.getStructure(0);
                 System.out.println(struct);
-//    		String name = struct.getName();
-//    		assertEquals("video/x-raw", name);
+    		String name = struct.getName();
+    		assertEquals("video/x-raw", name);
+                assertEquals(1, caps.size());
     	});
     }
 
